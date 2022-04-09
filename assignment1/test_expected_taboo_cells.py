59b2ba91d4e0cee2a36d1b4767448d6541bbe47e
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2022-04-09 13:22:14 +1000

    Improve execution time accuracy & include comments

--- assignment1/test_expected_taboo_cells.py
+++ assignment1/test_expected_taboo_cells.py
@@ -1,37 +1,61 @@
 from os import listdir
 from mySokobanSolver import taboo_cells
 from sokoban import Warehouse
-from time import perf_counter
+from time import perf_counter_ns
 
 def compare_taboo_cells(warehouse_file):
+    '''
+    
+
+    Parameters
+    ----------
+    warehouse_file : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+    TYPE
+        DESCRIPTION.
+
+    '''
     warehouse = Warehouse()
     
+    # Try load warehouse
     try:
         warehouse.load_warehouse("./warehouses/" + warehouse_file)
+    # If exception raised
     except Exception as e:
         print("\033[1;31m" + warehouse_file, "failed!\033[0;0m Could not load",
               "file -> " + repr(e) + ".")
         return (False, None)
     
-    start_time = perf_counter()
+    # Get and time answer
+    start_time = perf_counter_ns()
     answer = taboo_cells(warehouse)
-    execution_time = perf_counter() - start_time
+    execution_time = perf_counter_ns() - start_time
     
+    # Open expected answer file
     with open('./expected_taboo_cells/' + warehouse_file, 'r') as file:
         expected_answer = file.read()
 
+    # Characters forbidden from being in the taboo_cells output
     illegal_chars = ['$', '.', '@', '!', '*']
 
+    # If the expected file contains any forbidden characters
     if any(illegal_char in expected_answer for illegal_char in illegal_chars):
         print("\033[1;31m" + warehouse_file, "failed!\033[0;0m The expected",
               "answer file contains illegal characters, it most likely hasn't",
               "been configured yet.")
         return (False, None)
 
+    # If the generated answer matches the expected answer
     if answer == expected_answer:
         print(warehouse_file, "passed!")
         return (True, execution_time)
     
+    # Generated and expected answer do not match
     print("\033[1;31m" + warehouse_file, "failed!\n\033[0;0mExpected:\n" + 
           expected_answer + "\nActual:\n" + answer)
     return (False, None)
@@ -50,4 +74,4 @@ if __name__ == "__main__":
             pass_count += 1
             total_execution_time += result[1]
             
-    print("\n" + str(pass_count) + "/" + str(total_count), "tests passed. Average execution time:", round(total_execution_time/pass_count*1000, 4), "ms.")
+    print("\n" + str(pass_count) + "/" + str(total_count), "tests passed. Average execution time:", round(total_execution_time/pass_count/1000, 2), "μs.")
