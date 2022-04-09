98b575db232c142cf99ecc9c178559f663ef3e80
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2022-04-09 21:45:34 +1000

    Reimplement test taboo cells

--- test_taboo_cells.py
+++ test_taboo_cells.py
@@ -5,48 +5,37 @@ from sokoban import Warehouse
 from sys import stdout
 
 FORBIDDEN_CHARS = ['$', '.', '@', '!', '*']
-RESET = "\033[0;0m"
-YELLOW = "\033[1;33m"
+
+warehouse_file_list = listdir("./warehouses")
 
 class TestTabooCells(unittest.TestCase):
-    pass
-
-def test_generator(answer, expected_answer):
-    def test(self):
-        self.assertEqual(answer, expected_answer)
-        
-    return test
-
-if __name__ == "__main__":
-    warehouse_file_list = listdir("./warehouses")
-    
-    for warehouse_file in warehouse_file_list:
-        test_name = "test_{}".format(warehouse_file.replace(".txt", ""))
-        warehouse = Warehouse()
-        
-        # Try load warehouse
-        try:
-            warehouse.load_warehouse("./warehouses/" + warehouse_file)
-        except Exception as e:
-            print("{}Could not run {}. {}Warehouse file could not be loaded -> {}.".format(YELLOW, test_name, RESET, repr(e)))
-            stdout.flush()
-            continue
-        
-        # Get answer from function
-        answer = taboo_cells(warehouse)
-        
-        # Open expected answer file
-        with open('./expected_taboo_cells/' + warehouse_file, 'r') as file:
-            expected_answer = file.read()
+    def test_taboo_cells(self):
+        for warehouse_file in warehouse_file_list:
+            warehouse = Warehouse()
             
-        # If the expected answer contains any forbidden characters
-        if any(forbidden_char in expected_answer for forbidden_char in FORBIDDEN_CHARS):
-            print("{}Could not run {}. {}Expected answer is likely not configured.".format(YELLOW, test_name, RESET))
-            stdout.flush()
-            continue
-        
-        test_name = "test_{}".format(warehouse_file.replace(".txt", ""))
-        test = test_generator(answer, expected_answer)
-        setattr(TestTabooCells, test_name, test)
-        
+            # Try load warehouse
+            try:
+                warehouse.load_warehouse("./warehouses/" + warehouse_file)
+            except Exception as e:
+                print("Could not run test_taboo_cells subtest for {}. Warehouse file could not be loaded -> {}.".format(warehouse_file.replace(".txt", ""), repr(e)))
+                stdout.flush()
+                continue
+            
+            # Get answer from function
+            answer = taboo_cells(warehouse)
+            
+            # Open expected answer file
+            with open('./taboo_cells_expected/' + warehouse_file, 'r') as file:
+                expected_answer = file.read()
+                
+            # If the expected answer contains any forbidden characters
+            if any(forbidden_char in expected_answer for forbidden_char in FORBIDDEN_CHARS):
+                print("Could not run test_taboo_cells subtest for {}. Expected answer is likely not configured.".format(warehouse_file.replace(".txt", "")))
+                stdout.flush()
+                continue
+            
+            with self.subTest():
+                self.assertEqual(answer, expected_answer)
+                
+if __name__ == "__main__":
     unittest.main()
